926e2088e7b6d4a4a4754a06560262b664282d62
Samantha Laine <samanthalaine8@gmail.com>
2021-11-16 15:09:44 -0500

    solved

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,29 +1,15 @@
 function fibonacci(num) {
-  
+  if (num === 0) return 0
+  if (num === 1) return 1
+
+  return fibonacci(num-2)+ fibonacci(num-1)
 }
+console.log(fibonacci(10))
+
+
+// The mathematical formula to find the Fibonacci sequence number at a specific term is as follows:
+//Fn = Fn-1 + Fn-2
+//For a Fibonacci sequence, the base case is that the zeroth and first number of the sequence are 0 and 1 respectively
 
 
 
-
-
-
-// if (require.main === module) {
-//   // add your own tests in here
-//   console.log("Expecting: 0");
-//   console.log("=>", fibonacci(0));
-
-//   console.log("");
-
-//   console.log("Expecting: 1");
-//   console.log("=>", fibonacci(2));
-
-//   console.log("");
-
-//   console.log("Expecting: 55");
-//   console.log("=>", fibonacci(10));
-// }
-
-// module.exports = fibonacci;
-
-// Please add your pseudocode to this file
-// And a written explanation of your solution
