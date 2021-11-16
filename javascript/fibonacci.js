e09507e66ed1f793aa8940c3123daf2844e282d7
Samantha Laine <samanthalaine8@gmail.com>
2021-11-16 14:49:26 -0500

    start assignment

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,24 +1,29 @@
 function fibonacci(num) {
-  // type your code here
+  
 }
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 0");
-  console.log("=>", fibonacci(0));
 
-  console.log("");
 
-  console.log("Expecting: 1");
-  console.log("=>", fibonacci(2));
 
-  console.log("");
 
-  console.log("Expecting: 55");
-  console.log("=>", fibonacci(10));
-}
 
-module.exports = fibonacci;
+// if (require.main === module) {
+//   // add your own tests in here
+//   console.log("Expecting: 0");
+//   console.log("=>", fibonacci(0));
+
+//   console.log("");
+
+//   console.log("Expecting: 1");
+//   console.log("=>", fibonacci(2));
+
+//   console.log("");
+
+//   console.log("Expecting: 55");
+//   console.log("=>", fibonacci(10));
+// }
+
+// module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
